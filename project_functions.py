0a25b72fe725cca727fb73aa9f94cf332f0f00d8
Adam Chapdelaine <chapdela@email.uscb.edu>
2024-11-11 21:44:47 -0500

    Implemented function to read data file
    This commit includes the data's source file, as well as some experimentation with the pandas library. Not currently in workable status.

--- project_functions.py
+++ project_functions.py
@@ -1,7 +1,26 @@
+import csv
+import pandas as pd
 
+data = ("B104_Data_Sheet.csv")
 
+def readFile(file):
+    with open(file, mode= 'r') as file: # adding "as file" resolved issue with accessing internal file data 
+        reader = csv.reader(file)
+        for row in reader:
+            print(row[0])
 
-
-
-def readFile():
+def getColumns(file): # Attempting to create a dataframe with each column from the file, using pandas
+    with open(file, mode= 'r') as file:
+        reader = csv.reader(file)
+        df = pd.DataFrame([reader], index = None) 
+        for row in reader(df[1]):
+            print(row)
+            
+getColumns(data)
+  
     
+# readFile(data)
+
+
+# filedata = pd.read_csv(data)
+# print(filedata.corr()) # correlation function with pandas, currently bugged.
