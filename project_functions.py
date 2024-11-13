d8ab2ceed7a666db5420ab4faf3f5d6219691125
Adam Chapdelaine <chapdela@email.uscb.edu>
2024-11-12 22:36:35 -0500

    Implemented correlation function
    This commit also includes a function to retrieve a dataframe from the file, as well as a function to isolate columns from that dataframe.
    
    After some experimentation, I discovered that they can be rendered moot with a single line of code in any other function.
    
    Despite this, I decided to leave them be for future reference.

--- project_functions.py
+++ project_functions.py
@@ -1,26 +1,42 @@
 import csv
+import numpy as np
 import pandas as pd
 
 data = ("B104_Data_Sheet.csv")
 
-def readFile(file):
-    with open(file, mode= 'r') as file: # adding "as file" resolved issue with accessing internal file data 
-        reader = csv.reader(file)
-        for row in reader:
-            print(row[0])
-
-def getColumns(file): # Attempting to create a dataframe with each column from the file, using pandas
+def getDataframe(file): # creates a comprehensive dataframe of the contents of "B104_Data_Sheet.csv"
     with open(file, mode= 'r') as file:
-        reader = csv.reader(file)
-        df = pd.DataFrame([reader], index = None) 
-        for row in reader(df[1]):
-            print(row)
+        df = pd.read_csv(file)
+    return df
+    
+df = getDataframe(data)
+
+def isolateColumns(file): # Attempting to isolate columns from the dataframe, using pandas
+    df = pd.read_csv(file)
+    s = df[['q1','q2']] # Each successive column requires an additional set of brackets to avoid a KeyError
+    return s
+
+s = isolateColumns(data)
+
+def getCorrel(file): # produces the correlation between data in two or more columns
+    df = pd.read_csv(file)
+    print(df['qn84'].corr(df['qn85']))
             
-getColumns(data)
-  
+
+print(df)
+
+print(s)
+
+getCorrel(data)
     
 # readFile(data)
 
 
 # filedata = pd.read_csv(data)
 # print(filedata.corr()) # correlation function with pandas, currently bugged.
+
+# def readFile(file):
+#     with open(file, mode= 'r') as file: # adding "as file" resolved issue with accessing internal file data 
+#         reader = csv.reader(file)
+#         for row in reader:
+#             print(row[0])
