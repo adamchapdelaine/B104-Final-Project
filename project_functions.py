ec1703ae53eac2ac0a6a21712dbbf6bd5a26d299
Adam Chapdelaine <chapdela@email.uscb.edu>
2024-11-13 21:24:00 -0500

    fix: replace bad CSV file with corrected version
    This change fixes inaccuracies with the getCorrel function.
    
    Commit also refactors the getCorrel function to take additional parameters when called, making it more versatile.

--- project_functions.py
+++ project_functions.py
@@ -2,37 +2,23 @@ import csv
 import numpy as np
 import pandas as pd
 
-data = ("B104_Data_Sheet.csv")
+fileData = ("B104_Data_Sheet_v2.csv")
 
-def getDataframe(file): # creates a comprehensive dataframe of the contents of "B104_Data_Sheet.csv"
-    with open(file, mode= 'r') as file:
-        df = pd.read_csv(file)
-    return df
-    
-df = getDataframe(data)
-
-def isolateColumns(file): # Attempting to isolate columns from the dataframe, using pandas
+def getCorrel(file, var1, var2): # produces the correlation between data in two or more columns
     df = pd.read_csv(file)
-    s = df[['q1','q2']] # Each successive column requires an additional set of brackets to avoid a KeyError
-    return s
+    df = df.reset_index()
+    output = (df[f'{var1}'].corr(df[f'{var2}'])) # will now take any variable fed into the function
+    # print(df['q84'].corr(df['q85'])) # this line was just for debugging
+    return output
 
-s = isolateColumns(data)
+questionCorrel = getCorrel(fileData, 'q84', 'q85')
 
-def getCorrel(file): # produces the correlation between data in two or more columns
-    df = pd.read_csv(file)
-    print(df['qn84'].corr(df['qn85']))
-            
+print(questionCorrel) # negative correlation is greater now, sweet!
+   
 
-print(df)
+# *below is a bunch of junk code*
 
-print(s)
-
-getCorrel(data)
-    
-# readFile(data)
-
-
-# filedata = pd.read_csv(data)
+# filedata = pd.read_csv(fileData)
 # print(filedata.corr()) # correlation function with pandas, currently bugged.
 
 # def readFile(file):
@@ -40,3 +26,42 @@ getCorrel(data)
 #         reader = csv.reader(file)
 #         for row in reader:
 #             print(row[0])
+
+# def getDataframe(file): # creates a comprehensive dataframe of the contents of "B104_Data_Sheet.csv"
+#     with open(file, mode= 'r') as file:
+#         df = pd.read_csv(file)
+#     return df
+    
+# df = getDataframe(fileData)
+
+# def isolateColumns(file): # Attempting to isolate columns from the dataframe, using pandas
+#     df = pd.read_csv(file)
+#     s = df[['q1','q2']] # Each successive column requires an additional set of brackets to avoid a KeyError
+#     return s
+
+# s = isolateColumns(fileData)
+
+# readFile(data)
+
+# print(df)
+
+# print(s)
+
+
+# failed for loop attempt
+
+# for index, row in df.iterrows():
+#     if row['q1'] != 5:
+#       df.drop(row, errors='ignore')
+#     df2 = pd.concat()
+
+# def sliceDataframe(file):
+#     df = pd.read_csv(file)
+#     df2 = pd.DataFrame()
+    
+#     valueToRemove = 5
+    
+#     df2 = df[df['q1'] != valueToRemove]
+#     print(df2['q1'])
+    
+# sliceDataframe(fileData)   
