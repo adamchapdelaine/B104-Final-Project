9ccd991dc6d6989c69b811b3ced282732aefed2a
Adam Chapdelaine <chapdela@email.uscb.edu>
2024-11-22 16:54:48 -0500

    feat: add user interface in console
    Added a display function to present the user with a basic menu. The user can then input a number between 0-3 to see it's corresponding chart.
    
    Also cleaned up the script of depreciated code.

--- project_functions.py
+++ project_functions.py
@@ -15,21 +15,7 @@ df = pd.read_csv('B104_Data_Sheet_v2.csv')
 
 df.rename(columns= {'q1':'Age', 'q2':'Sex', 'q3':'Grade', 'q4':'Hispanic', 'q5':'Race', 'q6':'Height', 'q7':'Weight', 'q84':'Bad Mental Health', 'q85': 'Sleep Duration'}, inplace=True) 
 
-# #Changing the dataframe column "Sex" int values to strings that have significance
-# df['Sex'].replace([1.0, 2.0], ['Female', 'Male'], inplace = True)
-
-# # Changing
-# df['Grade'].replace([1.0, 2.0, 3.0, 4.0, 5.0], ['9th Grade', '10th Grade', '11th Grade', '12th Grade', 'Other'], inplace = True)
-
-
-# #Changing the dataframe column "Bad Mental Health" int values to strings that have significance
-
-# df['Bad Mental Health'].replace([1.0, 2.0, 3.0, 4.0, 5.0], ['Never', 'Rarely', 'Sometimes', 'Mostly', 'Always'], inplace = True)
-
-# #Changing the dataframe column "Sleep Duration" int values to strings that have significance
-
-# df['Sleep Duration'].replace([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0], ['≤ 4 hrs', '5 hrs', '6 hrs', '7 hrs', '8 hrs', '9 hrs', '≥ 10 hrs'], inplace = True)
-
+#-------------- Stage 3 --------------
 
 #Generates a pie chart with data taken from CSV file.
 def getPieCharts(): 
@@ -69,59 +55,14 @@ def getPieCharts():
 
 
     plt.show()
-
-def getMentalHealthPie():
-    print()
-
     
-
-    
-# def getBarChart():
-#     dfAltered = df.copy()
-#     dfAltered['Bad Mental Health'].replace([1.0, 2.0, 3.0, 4.0, 5.0], ['Never', 'Rarely', 'Sometimes', 'Mostly', 'Always'], inplace = True)
-#     dfAltered['Grade'].replace([1.0, 2.0, 3.0, 4.0, 5.0], ['9th Grade', '10th Grade', '11th Grade', '12th Grade', 'Other'], inplace = True)
-#     df4 = dfAltered[['Bad Mental Health', 'Grade']]
-    
-#     df4 = df4.replace(['Sometimes', 'Mostly', 'Always'],'Experiences bad mental health')
-    
-#     # Drop value 'Other' from grades
-#     df4 = df4.drop(df4[df4['Grade'] == 'Other'].index)
-    
-#     # Calculate minimum count for balancing before filtering
-#     minCount = df4['Grade'].value_counts().min()
-    
-#     # Create proportional sample
-#     balancedSample = df4.groupby('Grade').apply(lambda x: x.sample(minCount, random_state=42)).reset_index(drop=True)
-    
-#     # Filter for bad mental health experiences after balancing
-#     filteredDf = balancedSample[balancedSample['Bad Mental Health'] == 'Experiences bad mental health']
-    
-#     # Grouping "Bad Mental Health" to "Grade" for plotting student responses from each grade level
-#     groupedData = filteredDf['Grade'].value_counts().reset_index(name='count').rename(columns={'index': 'Grade'})
-    
-#     # Plotting the data
-#     sns.barplot(x='Grade', y='count', data=groupedData, order=['9th Grade', '10th Grade', '11th Grade', '12th Grade'])
-#     plt.xlabel('Grade')
-#     plt.ylabel('Count of Students')
-#     plt.title('Bad Mental Health by Grade (Sometimes or Greater)')
-#     plt.show()
-    
-#     # Print counts
-#     grouped_counts = filteredDf['Grade'].value_counts()
-#     print(grouped_counts)
-#     print(minCount)
-    
-    
-def getBarChart2():
+def getBarChart1():
     dfAltered = df.copy()
     dfAltered['Bad Mental Health'].replace([1.0, 2.0, 3.0, 4.0, 5.0], ['Never', 'Rarely', 'Sometimes', 'Mostly', 'Always'], inplace = True)
     dfAltered['Sex'].replace([1.0, 2.0], ['Female', 'Male'], inplace = True)
     df4 = dfAltered[['Bad Mental Health', 'Sex']]
     
     df4 = df4.replace(['Sometimes', 'Mostly', 'Always'],'Experiences bad mental health')
-    
-    # Drop value 'Other' from grades
-    # df4 = df4.drop(df4[df4['Grade'] == 'Other'].index)
     
     # Calculate minimum count for balancing before filtering
     minCount = df4['Sex'].value_counts().min()
@@ -142,12 +83,7 @@ def getBarChart2():
     plt.title('Bad Mental Health by Sex (Sometimes or Greater)')
     plt.show()
     
-    # # Print counts
-    # grouped_counts = filteredDf['Sex'].value_counts()
-    # print(grouped_counts)
-    # print(minCount)
-    
-def getBarChart3():
+def getBarChart2():
     dfAltered = df.copy()
     dfAltered['Bad Mental Health'].replace([1.0, 2.0, 3.0, 4.0, 5.0], ['Never', 'Rarely', 'Sometimes', 'Mostly', 'Always'], inplace = True)
     dfAltered['Sleep Duration'].replace([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0], ['≤ 4 hrs', '5 hrs', '6 hrs', '7 hrs', '8 hrs', '9 hrs', '≥ 10 hrs'], inplace = True)
@@ -185,116 +121,62 @@ def getBarChart3():
 def getHeatMap():
     df.rename(columns= {'q84':'Bad Mental Health', 'q85': 'Sleep Duration'}, inplace=True) 
     sns.heatmap(df.corr(numeric_only=True),  cmap="crest", annot=True)
-    plt.show
+    plt.show()
     
     # Note: Sex displays as negative correlation, could be reversed to positive, both are valid. Revisit later.
 
-#-------------- Stage 3 --------------
+def display():
+    print('\nChart Menu:')
+    print('--------------------------------------------------') 
+
+    # Task 4 Determine if the number provided by the user is prime
+    print('Options:\n\nPie Charts (Sleep & Mental Health) = 0\nBar Chart (Mental Health & Gender) = 1\nBar Chart (Sleep & Mental Health) = 2\nComprehensive Heatmap = 3')
+
+    while True:
+        userCheck = 'Y'
+        userInput = int(input('\nSelect 0-3 from the visual data list:\t'))
+        
+        if userInput == 0:
+            getPieCharts()
+            
+            userCheck = input('\nWould you like to continue? (Y/N):\t').lower()
+            if userCheck == ('y'):
+                continue
+            else:
+                break
+            
+            
+        elif userInput == 1:
+            getBarChart1()
+            
+            userCheck = input('Would you like to continue? (Y/N):\t').lower()
+            if userCheck == 'y':
+                continue
+            else:
+                break
+        
+        elif userInput == 2:
+            getBarChart2()
+            
+            userCheck = input('Would you like to continue? (Y/N):\t').lower()
+            if userCheck == 'y':
+                continue
+            else:
+                break
+            
+        elif userInput == 3:
+            getHeatMap()
+            
+            userCheck = input('Would you like to continue? (Y/N):\t').lower()
+            if userCheck == 'y':
+                continue
+            else:
+                break
+            
+        else:
+            print('Invalid selection. Please select a number between 0 and 3.') 
+    print('\nThank you for viewing this project.')
+
 #This is the start of our drop down menu 
 #-------------------------------------
-# # Task 2 'give user a informative statement.'    
-# print(f'Please enter a select data chart of your choice')
-# print('--------------------------------------------------')
-# print() #blank line 
-
-# # Task 4 Determine if the number provided by the user is prime
-# print("menu_options = ('0', '1', '2', '3')")
-
-# while True:
-#     userInput = int(input(f'Select 0-3 from the visual data list:\t'))
-    
-#     if userInput == 0:
-#         print('getPieCharts')
-#         break
-#     elif userInput == 1:
-#         print('getBarChart3')
-#         break
-#     elif userInput == 2:
-#         print('getHeatMap')
-#         break
-#     elif userInput == 3:
-#         print('getMentalHealthPie')
-#         break
-#     else:
-#         print(f'Invalid selection. Please select a number between 0 and 3')  
-        
-
-#-------------- Stage 4 --------------
-#Prints input data of Stage 2 and displays needed charts 
-getPieCharts()
-getBarChart2()
-getBarChart3()
-getHeatMap()
-
-# # def getCorrel(file, var1, var2): # produces the correlation between data in two or more columns
-# #     df = pd.read_csv(file)
-# #     df = df.reset_index()
-# #     output = (df[f'{var1}'].corr(df[f'{var2}'])) # will now take any variable fed into the function
-# #     # print(df['q84'].corr(df['q85'])) # this line was just for debugging
-# #     return output
-
-# questionCorrel = getCorrel(fileData, 'q84', 'q85')
-
-# print(questionCorrel) # negative correlation is greater now, sweet!
-   
-
-# def getHeatMap(file):
-    
-    
-    
-
-
-
-
-
-
-
-# # *below is a bunch of junk code*
-
-# # filedata = pd.read_csv(fileData)
-# # print(filedata.corr()) # correlation function with pandas, currently bugged.
-
-# # def readFile(file):
-# #     with open(file, mode= 'r') as file: # adding "as file" resolved issue with accessing internal file data 
-# #         reader = csv.reader(file)
-# #         for row in reader:
-# #             print(row[0])
-
-# # def getDataframe(dataFile): # creates a comprehensive dataframe of the contents of "B104_Data_Sheet.csv"
-# #     with open(file, mode= 'r') as file:
-# #         df = pd.read_csv(file)
-# #     return df
-    
-# # df = getDataframe(fileData)
-
-# # def isolateColumns(file): # Attempting to isolate columns from the dataframe, using pandas
-# #     df = pd.read_csv(file)
-# #     s = df[['q1','q2']] # Each successive column requires an additional set of brackets to avoid a KeyError
-# #     return s
-
-# # s = isolateColumns(fileData)
-
-# # readFile(data)
-
-# # print(df)
-
-# # print(s)
-
-
-# # failed for loop attempt
-
-# # for index, row in df.iterrows():
-# #     if row['q1'] != 5:
-# #       df.drop(row, errors='ignore')
-# #     df2 = pd.concat()
-
-# # def sliceDataframe(file):
-# #     df = pd.read_csv(file)
-# #     df2 = pd.DataFrame()
-    
-# #     valueToRemove = 5
-    
-# #     df2 = df[df['q1'] != valueToRemove]
-# #     print(df2['q1'])
-    
-# # sliceDataframe(fileData)   
+display() 
