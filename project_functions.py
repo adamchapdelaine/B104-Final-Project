4f95dd03a834877e2fb537e5df67ddd8455c0a73
Adam Chapdelaine <chapdela@email.uscb.edu>
2024-11-24 20:10:57 -0500

    Feat: add new bar chart
    Added a new bar chart that visualizes reported sleep duration by sex. Additionally, I revised the titles for the remaining charts.

--- project_functions.py
+++ project_functions.py
@@ -13,7 +13,7 @@ df = pd.read_csv('B104_Data_Sheet_v2.csv')
 #-------------- Stage 2 --------------
 # Changing the names of dataframe with corresponding colums
 
-df.rename(columns= {'q1':'Age', 'q2':'Sex', 'q3':'Grade', 'q4':'Hispanic', 'q5':'Race', 'q6':'Height', 'q7':'Weight', 'q84':'Bad Mental Health', 'q85': 'Sleep Duration'}, inplace=True) 
+df.rename(columns= {'q1':'Age', 'q2':'Sex', 'q3':'Grade', 'q4':'Hispanic', 'q5':'Race', 'q6':'Height', 'q7':'Weight', 'q84':'Poor MH', 'q85': 'Sleep Dur.'}, inplace=True) 
 
 #-------------- Stage 3 --------------
 
@@ -23,16 +23,17 @@ def getPieCharts():
     
     # 1st pie chart data
     dfAltered = df.copy()
-    dfAltered['Sleep Duration'].replace([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0], ['≤ 4 hrs', '5 hrs', '6 hrs', '7 hrs', '8 hrs', '9 hrs', '≥ 10 hrs'], inplace = True)
-    df2 = dfAltered['Sleep Duration']
+    dfAltered['Sleep Dur.'].replace([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0], ['≤ 4 hrs', '5 hrs', '6 hrs', '7 hrs', '8 hrs', '9 hrs', '≥ 10 hrs'], inplace = True)
+    df2 = dfAltered['Sleep Dur.']
     df2 = df2.replace(['≤ 4 hrs', '5 hrs', '6 hrs', '7 hrs'],"Less than 8 hrs")
     df2 = df2.replace(['9 hrs', '≥ 10 hrs'],"More than 8 hrs")
     
     # 2nd pie chart data
-    dfAltered['Bad Mental Health'].replace([1.0, 2.0, 3.0, 4.0, 5.0], ['Never', 'Rarely', 'Sometimes', 'Mostly', 'Always'], inplace = True)
-    df3 = dfAltered['Bad Mental Health']
-    df3 = df3.replace(['Never', 'Rarely'],'Rarely or never experiences bad mental health')
+    dfAltered['Poor MH'].replace([1.0, 2.0, 3.0, 4.0, 5.0], ['Never', 'Rarely', 'Sometimes', 'Mostly', 'Always'], inplace = True)
+    df3 = dfAltered['Poor MH']
     df3 = df3.replace(['Sometimes', 'Mostly', 'Always'],'Experiences bad mental health')
+    df3 = df3.replace(['Never', 'Rarely'],'Rarely or never experiences bad mental health')
+    
     
     # Establishing figure 
     fig, axs = plt.subplots(1, 2, figsize=(12, 9)) 
@@ -41,7 +42,7 @@ def getPieCharts():
     
     valueCounts1 = df2.value_counts()
     axs[0].pie(valueCounts1, colors = sns.color_palette("crest"), autopct='%1.1f%%', explode=None)
-    axs[0].set_title('Category 1 Distribution')
+    axs[0].set_title('Sleep Duration: Less, Equal, or More than 8 Hours')
     labels1 = ['Less than 8 hrs', '8 hrs', 'More than 8 hrs']
     axs[0].legend(labels = labels1, loc="upper right")
        
@@ -49,45 +50,59 @@ def getPieCharts():
     
     valueCounts2 = df3.value_counts()
     axs[1].pie(valueCounts2, colors = sns.color_palette("crest"), autopct='%1.1f%%', explode=None)
-    axs[1].set_title('Experiences bad Mental Health (Sometimes or Greater)')
-    labels2 = ['Rarely or Never', 'Sometimes or Greater']
+    axs[1].set_title('Frequency of Bad Mental Health: Common vs. Rare')
+    labels2 = ['Sometimes or Greater', 'Rarely or Never']
     axs[1].legend(labels = labels2, loc="upper right")
 
-
+    # Show the plots
     plt.show()
     
-def getBarChart1():
+def getBarChart1():  
+    # 1st bar chart data
     dfAltered = df.copy()
-    dfAltered['Bad Mental Health'].replace([1.0, 2.0, 3.0, 4.0, 5.0], ['Never', 'Rarely', 'Sometimes', 'Mostly', 'Always'], inplace = True)
-    dfAltered['Sex'].replace([1.0, 2.0], ['Female', 'Male'], inplace = True)
-    df4 = dfAltered[['Bad Mental Health', 'Sex']]
-    
-    df4 = df4.replace(['Sometimes', 'Mostly', 'Always'],'Experiences bad mental health')
-    
-    # Calculate minimum count for balancing before filtering
+    dfAltered['Poor MH'].replace([1.0, 2.0, 3.0, 4.0, 5.0], ['Never', 'Rarely', 'Sometimes', 'Mostly', 'Always'], inplace=True)
+    dfAltered['Sex'].replace([1.0, 2.0], ['Female', 'Male'], inplace=True)
+    df4 = dfAltered[['Poor MH', 'Sex']]
+    df4 = df4.replace(['Sometimes', 'Mostly', 'Always'], 'Experiences bad mental health')
+        # Establish balanced sample
     minCount = df4['Sex'].value_counts().min()
-    
-    # Create proportional sample
     balancedSample = df4.groupby('Sex').apply(lambda x: x.sample(minCount, random_state=42)).reset_index(drop=True)
-    
-    # Filter for bad mental health experiences after balancing
-    filteredDf = balancedSample[balancedSample['Bad Mental Health'] == 'Experiences bad mental health']
-    
-    # Grouping "Bad Mental Health" to "Grade" for plotting student responses from each grade level
+        # Filter for bad mental health experiences after balancing
+    filteredDf = balancedSample[balancedSample['Poor MH'] == 'Experiences bad mental health']
+        # Grouping "Bad Mental Health" to "Grade" for plotting student responses from each grade level
     groupedData = filteredDf['Sex'].value_counts().reset_index(name='count').rename(columns={'index': 'Sex'})
     
-    # Plotting the data, custom color palette (color added with 'palette' line 127)
-    sns.barplot(x='Sex', y='count', data=groupedData, order=['Female', 'Male'], palette="crest")
-    plt.xlabel('Sex')
-    plt.ylabel('Count of Students')
-    plt.title('Bad Mental Health by Sex (Sometimes or Greater)')
+    # 2nd bar chart data
+    dfAltered['Sleep Dur.'].replace([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0], ['≤ 4 hrs', '5 hrs', '6 hrs', '7 hrs', '8 hrs', '9 hrs', '≥ 10 hrs'], inplace=True)
+    df5 = dfAltered[['Poor MH', 'Sex', 'Sleep Dur.']]
+    df5 = df5.replace(['Sometimes', 'Mostly', 'Always'], 'Experiences bad mental health')
+        # Grouping for the second bar chart by Sleep Duration and Sex
+    sleepGroupedData = dfAltered.groupby(['Sleep Dur.', 'Sex']).size().reset_index(name='count')
+    
+    # Establishing figure
+    fig, axs = plt.subplots(1, 2, figsize=(12, 9))
+      
+    # Plotting the first bar chart
+    sns.barplot(x='Sex', y='count', data=groupedData, ax=axs[0], palette="crest")
+    axs[0].set_title('Reports of Bad Mental Health by Sex')
+    axs[0].set_xlabel('Sex')
+    axs[0].set_ylabel('Count of Students')
+    
+    # Plotting the second bar chart
+    sns.barplot(x='Sleep Dur.', y='count', hue='Sex', data=sleepGroupedData, ax=axs[1], palette="crest")
+    axs[1].set_title('Sleep Duration by Sex')
+    axs[1].set_xlabel('Sleep Duration')
+    axs[1].set_ylabel('Count of Students')
+    
+    # Show the plots
+    plt.tight_layout()
     plt.show()
     
 def getBarChart2():
     dfAltered = df.copy()
-    dfAltered['Bad Mental Health'].replace([1.0, 2.0, 3.0, 4.0, 5.0], ['Never', 'Rarely', 'Sometimes', 'Mostly', 'Always'], inplace = True)
-    dfAltered['Sleep Duration'].replace([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0], ['≤ 4 hrs', '5 hrs', '6 hrs', '7 hrs', '8 hrs', '9 hrs', '≥ 10 hrs'], inplace = True)
-    df4 = dfAltered[['Bad Mental Health', 'Sleep Duration']]
+    dfAltered['Poor MH'].replace([1.0, 2.0, 3.0, 4.0, 5.0], ['Never', 'Rarely', 'Sometimes', 'Mostly', 'Always'], inplace = True)
+    dfAltered['Sleep Dur.'].replace([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0], ['≤ 4 hrs', '5 hrs', '6 hrs', '7 hrs', '8 hrs', '9 hrs', '≥ 10 hrs'], inplace = True)
+    df4 = dfAltered[['Poor MH', 'Sleep Dur.']]
     
     df4 = df4.replace(['Sometimes', 'Mostly', 'Always'],'Experiences bad mental health')
     
@@ -95,31 +110,31 @@ def getBarChart2():
     # df4 = df4.drop(df4[df4['Grade'] == 'Other'].index)
     
     # Calculate minimum count for balancing before filtering
-    minCount = df4['Sleep Duration'].value_counts().min()
+    minCount = df4['Sleep Dur.'].value_counts().min()
     
     # Create proportional sample
-    balancedSample = df4.groupby('Sleep Duration').apply(lambda x: x.sample(minCount, random_state=42)).reset_index(drop=True)
+    balancedSample = df4.groupby('Sleep Dur.').apply(lambda x: x.sample(minCount, random_state=42)).reset_index(drop=True)
     
     # Filter for bad mental health experiences after balancing
-    filteredDf = balancedSample[balancedSample['Bad Mental Health'] == 'Experiences bad mental health']
+    filteredDf = balancedSample[balancedSample['Poor MH'] == 'Experiences bad mental health']
     
     # Grouping "Bad Mental Health" to "Grade" for plotting student responses from each grade level
-    groupedData = filteredDf['Sleep Duration'].value_counts().reset_index(name='count').rename(columns={'index': 'Sleep Duration'})
+    groupedData = filteredDf['Sleep Dur.'].value_counts().reset_index(name='count').rename(columns={'index': 'Sleep Dur.'})
     
     # Plotting the data
-    sns.barplot(x='Sleep Duration', y='count', data=groupedData, order=['≤ 4 hrs', '5 hrs', '6 hrs', '7 hrs', '8 hrs', '9 hrs', '≥ 10 hrs'], palette="crest")
+    sns.barplot(x='Sleep Dur.', y='count', data=groupedData, order=['≤ 4 hrs', '5 hrs', '6 hrs', '7 hrs', '8 hrs', '9 hrs', '≥ 10 hrs'], palette="crest")
     plt.xlabel('Hours of Sleep')
     plt.ylabel('Count of Students')
-    plt.title('Bad Mental Health by Sleep (Sometimes or Greater)')
+    plt.title('Reports of Bad Mental Health by Hours of Sleep')
     plt.show()
     
     # # Print counts
-    # grouped_counts = filteredDf['Sleep Duration'].value_counts()
+    # grouped_counts = filteredDf['Sleep Dur.'].value_counts()
     # print(grouped_counts)
     # print(minCount)
     
 def getHeatMap():
-    df.rename(columns= {'q84':'Bad Mental Health', 'q85': 'Sleep Duration'}, inplace=True) 
+    df.rename(columns= {'q84':'Poor MH', 'q85': 'Sleep Dur.'}, inplace=True) 
     sns.heatmap(df.corr(numeric_only=True),  cmap="crest", annot=True)
     plt.show()
     
